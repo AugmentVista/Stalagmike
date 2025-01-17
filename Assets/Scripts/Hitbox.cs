ac7695b5fb922d830a6cbca45eebd2c1330802df
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-17 14:31:29 -0400

    Add Hitboxes

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+internal class Hitbox:MonoBehaviour
+{
+    Collider2D trigger;
+
+    // public Action<HealthSystem> OnHit = delegate { };
+
+    private void OnEnable()
+    {
+        if (trigger == null) trigger = GetComponent<Collider2D>();
+        trigger.isTrigger = true;
+        trigger.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        trigger.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if (collision.TryGetComponent(out HealthSystem target)) { OnHit(target); }
+    }
+}
