9777cc54e063b23b3315fcdc72a66e99ed185f3c
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-17 19:06:39 -0400

    Start the health system

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class HealthSystem:MonoBehaviour
+    {
+        public Action OnDeath = delegate { };
+        public int health { get; protected set; } = 1;
+        
+
+        public void TakeDamage(HitInfo hit)
+        {
+            health -= hit.damage;
+
+            if (health < 0) { OnDeath(); }
+        }
+    }
+}
---
+++ Assets/Scripts/Struct/HitInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Struct
+{
+    /// <summary>
+    /// Represents a damaging hit dealt from one object to another.
+    /// </summary>
+    internal struct HitInfo
+    {
+        public int damage;
+
+        public HitInfo(int damage)
+        {
+            this.damage = damage;
+        }
+
+        // While we only track damage, this will make things easier, however we're using a struct for extendability.
+        public static implicit operator HitInfo(int damage) {  return new HitInfo(damage); }
+        public static implicit operator int(HitInfo hitInfo) { return hitInfo.damage; }
+    }
+}
