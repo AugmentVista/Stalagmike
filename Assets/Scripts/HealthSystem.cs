617285a016f3e0662df01f9f582cdf674b542e41
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-18 19:12:56 -0400

    Update HealthSystem.cs

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -10,8 +10,14 @@ namespace Assets.Scripts
 {
     internal class HealthSystem:MonoBehaviour
     {
+        /// <summary>
+        /// If disabled, we wont spam the logs when OnDeath is called.
+        /// </summary>
         [SerializeField] bool debugMe = true;
         [SerializeField] int maxHp = 1;
+        /// <summary>
+        /// Called when reaching 0 hp.
+        /// </summary>
         public Action OnDeath;
         public int health { get; protected set; } = 0;
 
@@ -23,6 +29,10 @@ namespace Assets.Scripts
             OnDeath += delegate { if (debugMe) { Debug.Log($"{name} was killed."); health = maxHp; } };
         }
 
+        /// <summary>
+        /// Makes this HealthSystem take damage. If hp falls to or below 0, OnDeath is called.
+        /// </summary>
+        /// <param name="hit">The data associated with the hit we're taking.</param>
         public void TakeDamage(HitInfo hit)
         {
             health -= hit.damage;
