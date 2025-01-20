fc110d8cb775e8e504674e90acb3fd5c1630a35d
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-01-20 13:37:09 -0400

    Update PlayerController.cs
    Split attack stuff into separate script.

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -10,6 +10,7 @@ class PlayerController : MonoBehaviour
     [SerializeField] GroundDetector groundDetector;
     InputManager inputManager = new();
     Rigidbody2D rb;
+    AbilityController abilityController;
     Action PhysicsProcess = delegate { };
 
     [Header("Movement Stats")]
@@ -26,96 +27,22 @@ class PlayerController : MonoBehaviour
     int jumpTicksLeft = 0;
     //#endregion
 
-    [Header("Melee Stats")]
-    [SerializeField] Hitbox meleeHitbox;
-    [SerializeField] HitInfo meleeHitInfo;
-    [SerializeField] int meleeCooldownTicks = 15;
-    [SerializeField] int meleeActiveTicks = 5;
-
-    [Header("Ranged Stats")]
-    [SerializeField] GameObject ammo;
-    [SerializeField] HitInfo rangedHitInfo;
-    [SerializeField] int rangedCooldownTicks = 30;
-
-    // Misc Attack Stuff
-    int attackCooldown = 0;
-
     private void Start()
     {
         // Event stuff.
         groundDetector.Landed += () => { grounded = true; };
-        meleeHitbox.OnHit += OnMeleeHit;
     }
 
     private void OnEnable()
     {
         rb = GetComponent<Rigidbody2D>();
+        abilityController = GetComponent<AbilityController>();
     }
 
     private void FixedUpdate()
     {
         HandleMovement();
-        HandleAttacks();
         PhysicsProcess();
-    }
-
-    private void HandleAttacks()
-    {
-        //Debug.Log($"Is Atk pressed? {inputManager.standardAttackInput}. Is SpA pressed? {inputManager.specialAttackInput}.");
-
-        // If there is no cooldown, check for attack input. Otherwise, decrement the cooldown.
-        if (attackCooldown < 1)
-        {
-            // If we have an attack input, use the attack and set the cooldown counter to the relevant cooldown length.
-            if (inputManager.standardAttackInput) { UseMeleeAtk(); attackCooldown = meleeCooldownTicks; }
-            else if (inputManager.specialAttackInput) { UseRangedAtk(); attackCooldown = rangedCooldownTicks; }
-        }
-        else { attackCooldown--; }
-    }
-
-    private void UseMeleeAtk()
-    {
-        bool shouldRotate;
-        // Determine if we need to rotate the hitbox. If our last motion was backwards, we should be flipped, and our hitbox should be too.
-        float vel = rb.velocity.x;
-        if (vel > 0) { shouldRotate = false; }
-        else if (vel < 0) { shouldRotate = true; }
-        else { shouldRotate = IsFlipped; }
-
-        // Create a vector3 to set our rotation.
-        Vector3 angles = Vector3.zero;
-        if (shouldRotate) { angles = new Vector3(0, 180, 0); }
-        meleeHitbox.transform.localEulerAngles = angles;
-        meleeHitbox.enabled = true;
-
-        // Set a timer to count down how many ticks the attack hitbox will be active if it doesn't hit anything.
-        int activeTicksLeft = meleeActiveTicks;
-
-        // Use a method so we can deactivate and unsubscribe later.
-        void AtkProcess()
-        {
-            //Debug.Log("ticks left = " + activeTicksLeft);
-            activeTicksLeft--;
-            if (activeTicksLeft <= 0)
-            {
-                meleeHitbox.enabled = false;
-                PhysicsProcess -= AtkProcess;
-            }
-        }
-
-        // Add the method.
-        PhysicsProcess += AtkProcess;
-    }
-
-    private void OnMeleeHit(HealthSystem target)
-    {
-        target.TakeDamage(meleeHitInfo);
-        meleeHitbox.enabled = false;
-    }
-
-    private void UseRangedAtk()
-    {
-        throw new NotImplementedException();
     }
 
     private void HandleMovement()
