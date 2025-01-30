a77bc10095fd383eee6aab6c37dea9a0c54f6ab9
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-30 17:56:56 -0400

    Create DoT.cs

---
+++ Assets/Scripts/Entity/Util/DoT.cs
@@ -0,0 +1,24 @@
+﻿namespace Assets.Scripts.Entity.Util
+{
+    /// <summary>
+    /// Represents a damage over time effect.
+    /// </summary>
+    internal struct DoT
+    {
+        /// <summary>
+        /// How many more times should this effect be ticked?
+        /// </summary>
+        public int TicksLeft { get; private set; }
+        int damage;
+
+        /// <summary>
+        /// Gets the damage from this Damage over Time instance, and decrements the timer.
+        /// </summary>
+        /// <returns>The amount of damage to be dealt.</returns>
+        public int Tick()
+        {
+            TicksLeft--;
+            return damage;
+        }
+    }
+}
