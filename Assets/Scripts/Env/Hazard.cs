4d703a4bc0e63dce13b89d1422ce3c22d160b651
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-30 21:13:50 -0400

    Create Hazard.cs

---
+++ Assets/Scripts/Env/Hazard.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Struct;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Env
+{
+    /// <summary>
+    /// Represents an object that can interact with entities.
+    /// </summary>
+    internal class Hazard : MonoBehaviour
+    {
+        [SerializeField] HitInfo damageInfo;
+        List<HealthSystem> blerg = new();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out HealthSystem healthSystem))
+            {
+                if (!blerg.Contains(healthSystem)) { blerg.Add(healthSystem); }
+            }
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out HealthSystem healthSystem))
+            {
+                if (blerg.Contains(healthSystem)) { blerg.Remove(healthSystem); }
+            }
+        }
+        private void FixedUpdate()
+        {
+            foreach (HealthSystem healthSystem in blerg)
+            {
+                healthSystem.TakeDamage(damageInfo);
+            }
+        }
+    }
+}
