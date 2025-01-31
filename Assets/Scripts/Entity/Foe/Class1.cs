077aaaf3583e6bbccf06165de433aca03384cb6e
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-01-31 11:27:18 -0400

    Blerg

---
+++ Assets/Scripts/Entity/Foe/Class1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Entity.Foe
+{
+    internal class Class1:MonoBehaviour
+    {
+        [SerializeField] foebehavior patrol, chase;
+        PlayerDetector playerDetector;
+        int stateInt;
+
+        void Ready()
+        {
+            playerDetector = GetComponent<PlayerDetector>();
+            chase.detector = playerDetector;
+        }
+
+        void PhysicsProcess()
+        {
+            switch (stateInt)
+            {
+                case 0: patrol.Execute(); break;
+                case 1: chase.Execute(); break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Entity/Foe/foebehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assets.Scripts.Entity.Foe
+{
+    internal class foebehavior
+    {
+        internal PlayerDetector detector;
+
+        internal virtual void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
