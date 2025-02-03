31907390632b32f848616228e3bc6b5f54ed95ab
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 17:25:37 -0400

    Add attack logic.

--- Assets/Scripts/Entity/Foe/Behaviors/AttackBehavior.cs
+++ Assets/Scripts/Entity/Foe/Behaviors/AttackBehavior.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Assets.Scripts.Entity.Util;
+using System;
+using UnityEngine;
 
 namespace Assets.Scripts.Entity.Foe.Behaviors
 {
     internal class AttackBehavior:FoeBehavior
     {
+        [SerializeField] protected HitInfo hit;
         internal Hitbox hitbox;
 
         internal override void Execute()
@@ -12,6 +15,16 @@ namespace Assets.Scripts.Entity.Foe.Behaviors
             base.Execute();
 
             hitbox.enabled = true;
+        }
+
+        internal override void Init()
+        {
+            hitbox.OnHit += OnHit;
+        }
+
+        protected virtual void OnHit(HealthSystem healthSystem)
+        {
+            healthSystem.TakeDamage(hit);
         }
     }
 }
