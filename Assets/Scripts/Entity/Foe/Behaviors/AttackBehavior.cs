512c0d444b40233ae74fc0f35d345d19beb82c43
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 17:13:11 -0400

    Update AttackBehavior.cs
    Added atk logic

--- Assets/Scripts/Entity/Foe/Behaviors/AttackBehavior.cs
+++ Assets/Scripts/Entity/Foe/Behaviors/AttackBehavior.cs
@@ -4,5 +4,14 @@ namespace Assets.Scripts.Entity.Foe.Behaviors
 {
     internal class AttackBehavior:FoeBehavior
     {
+        internal Hitbox hitbox;
+
+        internal override void Execute()
+        {
+            // Keep the warning here.
+            base.Execute();
+
+            hitbox.enabled = true;
+        }
     }
 }
