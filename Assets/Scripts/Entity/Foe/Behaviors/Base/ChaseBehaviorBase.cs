0b5fe06c38ef6a126b4fd8cccf7c0a22c5441615
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 19:22:48 -0400

    add movement vars to chase base

--- Assets/Scripts/Entity/Foe/Behaviors/Base/ChaseBehaviorBase.cs
+++ Assets/Scripts/Entity/Foe/Behaviors/Base/ChaseBehaviorBase.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Entity.Foe.Behaviors
 {
     internal class ChaseBehaviorBase:FoeBehavior
     {
         internal Action<Class1> Attack;
+
+        [SerializeField] protected float targetSpeed;
+        [SerializeField] protected float accel; // as a factor of target velocity
     }
 }
