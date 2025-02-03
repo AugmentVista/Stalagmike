a458bebfa01ccd68ad4282fdb6547c5d00eaaae5
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 19:26:35 -0400

    Create Chase1.cs

---
+++ Assets/Scripts/Entity/Foe/Behaviors/Chase1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Scripts.Entity.Foe.Behaviors
+{
+    [CreateAssetMenu(menuName = "FoeBehaviors")]
+    internal class Chase1:ChaseBehaviorBase
+    {
+        internal override void Execute(Class1 parent)
+        {
+            // get ref
+            Rigidbody2D rb = parent.GetComponent<Rigidbody2D>();
+            // calculate target horizontal direction as a float. (we really only care about its sign.)
+            float hPos = rb.position.x;
+            float targetHVel = hPos - player.transform.position.x;
+            // calculate new velocity and apply it.
+            Vector2 targetVel = new Vector2(targetHVel, 0).normalized * targetSpeed;
+            rb.velocity = Vector2.Lerp(rb.velocity, targetVel, accel);
+        }
+
+        protected override void OnPlayerDetected(PlayerController player)
+        {
+            Attack(null); // THIS IS BAD, FIX SOON!
+        }
+    }
+}
