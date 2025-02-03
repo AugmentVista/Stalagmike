b2787ab19519aa3329b4eb1a201ae314586bbc5e
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 19:09:32 -0400

    Create Patrol1.cs

---
+++ Assets/Scripts/Entity/Foe/Behaviors/Patrol1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Entity.Foe.Behaviors
+{
+    internal class Patrol1:PatrolBehaviorBase
+    {
+        internal override void Execute(Class1 parent)
+        {
+            // get ref
+            Rigidbody2D rb = parent.GetComponent<Rigidbody2D>();
+            // calculate target horizontal direction as a float. (we really only care about its sign.)
+            float hPos = rb.position.x;
+            float targetHVel = isForward ? hPos - start.x : hPos - end.x;
+            // calculate new velocity and apply it.
+            Vector2 targetVel = new Vector2(targetHVel, 0).normalized * targetSpeed;
+            rb.velocity = Vector2.Lerp(rb.velocity, targetVel, accel);
+        }
+    }
+}
