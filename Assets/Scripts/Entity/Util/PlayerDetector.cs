aa8fe7058d2cbae1b4ffe82d9bd6f6f65c45c5b6
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 17:04:04 -0400

    Add detection logic.

--- Assets/Scripts/Entity/Util/PlayerDetector.cs
+++ Assets/Scripts/Entity/Util/PlayerDetector.cs
@@ -1,6 +1,18 @@
-﻿namespace Assets.Scripts.Entity.Foe
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Entity.Foe
 {
-    internal class PlayerDetector
+    /// <summary>
+    /// Detects if a player is within a certain area, and reports on enter.
+    /// </summary>
+    internal class PlayerDetector:MonoBehaviour
     {
+        public Action<PlayerController> PlayerDetected = delegate { };
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.TryGetComponent(out PlayerController controller)) {  PlayerDetected(controller); }
+        }
     }
 }
