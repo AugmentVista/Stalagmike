182d9fa5a7e64e3e68071046a06545ef395d1135
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 17:15:58 -0400

    Add init

--- Assets/Scripts/Entity/Foe/Behaviors/FoeBehavior.cs
+++ Assets/Scripts/Entity/Foe/Behaviors/FoeBehavior.cs
@@ -11,5 +11,10 @@ namespace Assets.Scripts.Entity.Foe.Behaviors
         {
             Debug.LogWarning(new NotImplementedException("FoeBehavior.Execute() should be overridden."));
         }
+
+        internal virtual void Init()
+        {
+            Debug.Log("This should probably be overridden.");
+        }
     }
 }
