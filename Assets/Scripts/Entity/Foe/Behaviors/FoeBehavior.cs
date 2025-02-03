d6a0633cbd16dd12497609d640e5ad5e248b5124
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-02-03 17:46:34 -0400

    Update FoeBehavior.cs

--- Assets/Scripts/Entity/Foe/Behaviors/FoeBehavior.cs
+++ Assets/Scripts/Entity/Foe/Behaviors/FoeBehavior.cs
@@ -5,6 +5,7 @@ namespace Assets.Scripts.Entity.Foe.Behaviors
 {
     internal abstract class FoeBehavior
     {
+        internal Action<Class1.AIState> SetState;
         internal PlayerDetector playerDetector;
 
         internal virtual void Execute()
