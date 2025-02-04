4ef818f76e5697d7c301cdf59e57219d026f1625
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-02-04 17:02:46 -0400

    Static the InputMan

--- Assets/Scripts/Entity/Player/InputManager.cs
+++ Assets/Scripts/Entity/Player/InputManager.cs
@@ -3,20 +3,21 @@
 /// <summary>
 /// Used to read player input.
 /// </summary>
-internal class InputManager
+internal static class InputManager
 {
     /// <summary>
     /// -1 if A held, 1 if D held, 0 if neither or both are held.
     /// </summary>
-    public float moveValue { get { return ReadMoveInput(); } }
+    public static float moveValue { get { return ReadMoveInput(); } }
     /// <summary>
     /// Is the player pressing space?
     /// </summary>
-    public bool jumpInput { get { return Input.GetKey(KeyCode.Space); } }
-    public bool standardAttackInput { get { return Input.GetMouseButton(0); } }
-    public bool specialAttackInput { get { return Input.GetMouseButton(1); } }
+    public static bool jumpInput { get { return Input.GetKey(KeyCode.Space); } }
+    public static bool standardAttackInput { get { return Input.GetMouseButton(0); } }
+    public static bool specialAttackInput { get { return Input.GetMouseButton(1); } }
+    public static bool pauseInput { get { return Input.GetKey(KeyCode.Escape); } }
 
-    private float ReadMoveInput()
+    private static float ReadMoveInput()
     {
         float value = 0;
         if (Input.GetKey(KeyCode.A)) value--;
