a985c86b50f3ee263ba4884c0628c537d16f98fa
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-03-17 09:07:00 -0300

    Create WallDetector.cs

---
+++ Assets/Scripts/Entity/Util/WallDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Entity.Util
+{
+    /// <summary>
+    /// Used for walljumps.
+    /// </summary>
+    internal class WallDetector:GroundDetector
+    {
+    }
+}
