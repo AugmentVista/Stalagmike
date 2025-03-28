360a26440856474f0e83a860b7356ca4f5e61281
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-03-28 12:44:45 -0300

    Wrote the pickup thingy

---
+++ Assets/Scripts/Pickups/HPPickup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Pickups
+{
+    class HPPickup:MonoBehaviour
+    {
+        [SerializeField]int value;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(TryGetComponent(out HealthSystem hs)&&TryGetComponent(out PlayerController ignored))
+            {
+                // Deal negative damage to make it quicker.
+                hs.TakeDamage(value * -1);
+            }
+        }
+    }
+}
