15730cb890a269daad82126733a916340fb0044f
Silver Burnie <AuAgments@gmail.com>
2025-03-28 14:45:59 -0300

    checkpoint flags implemented

--- Assets/Scripts/Env/CheckPointTrigger.cs
+++ Assets/Scripts/Env/CheckPointTrigger.cs
@@ -1,5 +1,5 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CheckPointTrigger : MonoBehaviour
 {
@@ -9,24 +9,68 @@ public class CheckPointTrigger : MonoBehaviour
 
     public Transform newRespawnPoint;
 
+    [Header("Flag Sprites for Each Checkpoint")]
+    [SerializeField] private List<Sprite> limpFlags = new List<Sprite>();   // 3 Limp flag sprites
+    [SerializeField] private List<Sprite> flowingFlags = new List<Sprite>(); // 3 Flowing flag sprites
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             GameObject player = collision.gameObject;
-            // Get a reference to the player gameObject
 
-            if (!targetRespawnPoint.activeSelf) { targetRespawnPoint.SetActive(true); }
-            // Ensure the respawn point we want is active
+            // Ensure the target respawn point is active
+            if (!targetRespawnPoint.activeSelf)
+            {
+                targetRespawnPoint.SetActive(true);
+            }
 
+            // Set new respawn point
             newRespawnPoint = targetRespawnPoint.transform;
-            // Assign a value to newRespawnPoint to our target respawnPoint
 
+            // Update player's respawn point
             if (player.TryGetComponent(out PlayerController playerController))
             {
-                // Tell the playerController to use the new value for its respawn location
-                playerController.RespawnPoint = newRespawnPoint.transform;
+                playerController.RespawnPoint = newRespawnPoint;
+            }
+
+            // Swap sprite of targetRespawnPoint to the corresponding flowing flag
+            UpdateFlagSprite(targetRespawnPoint, true);
+
+            // Reset other flags to their limp versions
+            ResetOtherFlags(targetRespawnPoint);
+        }
+    }
+
+    private void UpdateFlagSprite(GameObject checkpoint, bool isFlowing)
+    {
+        SpriteRenderer flagRenderer = checkpoint.GetComponentInChildren<SpriteRenderer>();
+
+        if (flagRenderer != null)
+        {
+            int index = respawnPoints.IndexOf(checkpoint);
+            if (index >= 0 && index < respawnPoints.Count)
+            {
+                flagRenderer.sprite = isFlowing ? flowingFlags[index] : limpFlags[index];
+            }
+        }
+    }
+
+    private void ResetOtherFlags(GameObject activeCheckpoint)
+    {
+        for (int i = 0; i < respawnPoints.Count; i++)
+        {
+            GameObject respawnPoint = respawnPoints[i];
+
+            if (respawnPoint != activeCheckpoint)
+            {
+                SpriteRenderer flagRenderer = respawnPoint.GetComponentInChildren<SpriteRenderer>();
+                if (flagRenderer != null && i < limpFlags.Count)
+                {
+                    flagRenderer.sprite = limpFlags[i];
+                }
             }
         }
     }
 }
+
