b99782e1fda2bebb9f1fcc3fb08ea5646e1fd70b
Silver Burnie <AuAgments@gmail.com>
2025-04-11 00:21:55 -0300

    PlayerUIManager overhaul
    PlayerUIManager now dynamically creates, and manages the heart icons.
    
    Player's starting health must be divisible by 2.

--- Assets/Scripts/Entity/Player/PlayerUIManager.cs
+++ Assets/Scripts/Entity/Player/PlayerUIManager.cs
@@ -1,34 +1,166 @@
 using Assets.Scripts;
 using Assets.Scripts.Entity.Util;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 
 public class PlayerUIManager : MonoBehaviour
 {
+    private class HeartPair
+    {
+        public GameObject leftHeart;
+        public GameObject rightHeart;
+    }
+
+    private List<HeartPair> heartPairs = new List<HeartPair>();
+
     [SerializeField] private HealthSystem healthSystem;
 
-    [SerializeField] private List<GameObject> hearts = new List<GameObject>();
+    private List<GameObject> listOfFullHearts = new List<GameObject>();
 
+    [SerializeField] private GameObject fullHeartPrefab;
+
+    GameObject player;
+
+    int lastKnownMaxHP;
+    int lastKnownHealth;
 
     private void Start()
     {
-        healthSystem = GetComponent<HealthSystem>();
-        //Debug.Log($"Is HealthSystem Valid? {healthSystem.isActiveAndEnabled}");
-        //Debug.Log(hearts.Count.ToString());
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null && player.TryGetComponent(out HealthSystem playerHealthSystem))
+        {
+            healthSystem = playerHealthSystem;
+            lastKnownMaxHP = healthSystem.maxHp;
+            lastKnownHealth = healthSystem.health;
+        }
+        else
+        {
+            Debug.LogWarning("Player or HealthSystem not found by UI script.");
+        }
+    }
+
+    public void SetMaxUIHealth()
+    {
+        int fullHeartCount = healthSystem.maxHp / 2;
+        Vector3 heartPositions = new Vector3(0, 0, 0);
+
+        for (int i = 0; i < fullHeartCount; i++)
+        {
+            heartPositions.x *= i * 100;
+            GameObject fullheart = Instantiate(fullHeartPrefab, heartPositions, Quaternion.identity);
+            listOfFullHearts.Add(fullheart);
+        }
+    }
+
+    private void UpdateMaxHealth()
+    {
+        if (lastKnownMaxHP + 1 < healthSystem.maxHp)
+        {
+            int deltaMaxHealth = healthSystem.maxHp - lastKnownMaxHP;
+            lastKnownMaxHP = healthSystem.maxHp;
+            Vector3 heartPositions = new Vector3(0, 0, 0);
+
+            for (int i = 0; i < deltaMaxHealth; i++)
+            {
+                // Use the amount of hearts to determine the position of the next heart.
+                heartPositions.x = listOfFullHearts.Count * 100 + i * 100;
+                GameObject fullheart = Instantiate(fullHeartPrefab, heartPositions, Quaternion.identity);
+                listOfFullHearts.Add(fullheart);
+
+                // Get reference to the halves of the heart that was just created
+                Transform left = fullheart.transform.Find("Left Heart");
+                Transform right = fullheart.transform.Find("Right Heart");
+
+                if (left == null || right == null)
+                {
+                    Debug.LogWarning($"Heart prefab is missing expected child objects: 'Left Heart' or 'Right Heart' ");
+                    continue;
+                }
+
+                // create an instance of HeartPair class named pair
+                HeartPair pair = new HeartPair
+                {
+                    leftHeart = left.gameObject,
+                    rightHeart = right.gameObject
+                };
+
+                // add pair to a list of heartPairs
+                heartPairs.Add(pair);
+            }
+        }
     }
 
     private void Update()
     {
-        if (healthSystem.isActiveAndEnabled && healthSystem.health <= healthSystem.maxHp)
+        UpdateHealthValue();
+    }
+
+    private void UpdateHealthValue()
+    {
+        // if delta health is postive player gained health, if negative player lost health
+        int deltaHealth = healthSystem.health - lastKnownHealth;
+
+        if (deltaHealth != 0)
         {
-            foreach (GameObject heart in hearts)
+            if (deltaHealth > 0)
+                ApplyHealing(deltaHealth); 
+
+            if (deltaHealth < 0)
+                ApplyDamage(deltaHealth);
+
+            lastKnownHealth = healthSystem.health;
+        }
+    }
+
+    public void ApplyDamage(int damageToApply)
+    {
+        int damageTaken = 0;
+
+        // Start from the end of the list and go backwards by decrementing i
+        for (int i = heartPairs.Count - 1; i >= 0 && damageTaken < damageToApply; i--)
+        {
+            HeartPair pair = heartPairs[i];
+            Image rightHeartImage = pair.rightHeart.GetComponent<Image>();
+            Image leftHeartImage = pair.leftHeart.GetComponent<Image>();
+
+            if (damageTaken < damageToApply && rightHeartImage.enabled)
             {
-                heart.SetActive(false);
+                rightHeartImage.enabled = false;
+                damageTaken++;
             }
-            for (int i = 0; i < healthSystem.health; i++)
+
+            if (damageTaken < damageToApply && leftHeartImage.enabled)
             {
-                hearts[i].SetActive(true);
+                leftHeartImage.enabled = false;
+                damageTaken++;
+            }
+        }
+    }
+
+    public void ApplyHealing(int healingToApply)
+    {
+        int healingTaken = 0;
+
+        // Start from the end of the list and go backwards by decrementing i
+        for (int i = heartPairs.Count - 1; i >= 0 && healingTaken < healingToApply; i--)
+        {
+            HeartPair pair = heartPairs[i];
+            Image rightHeartImage = pair.rightHeart.GetComponent<Image>();
+            Image leftHeartImage = pair.leftHeart.GetComponent<Image>();
+
+            if (healingTaken < healingToApply && !leftHeartImage.enabled)
+            {
+                leftHeartImage.enabled = true;
+                healingTaken++;
+            }
+
+            if (healingTaken < healingToApply && !rightHeartImage.enabled)
+            {
+                rightHeartImage.enabled = true;
+                healingTaken++;
             }
         }
     }
--- Assets/Scripts/Pickups/HPPickup.cs
+++ Assets/Scripts/Pickups/HPPickup.cs
@@ -6,13 +6,16 @@ namespace Assets.Scripts.Pickups
     {
         [SerializeField] int value;
 
+        // This must be a factor of 2
+        [SerializeField] int maxHealthIncrease = 2;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.TryGetComponent(out HealthSystem hs) && collision.TryGetComponent(out PlayerController ignored))
             {
                 // Deal negative damage to make it quicker.
                 hs.TakeDamage(value * -1);
-                hs.maxHp++;
+                hs.maxHp += maxHealthIncrease;
                 Destroy(gameObject);
             }
         }
